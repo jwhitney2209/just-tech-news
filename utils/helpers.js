3e0f87cb7614b0b56adce9b57ff355bc67fa154b
teachergg <jwhitney@bisd.us>
2022-05-04 18:35:39 -0500

    added edit and delete functionality

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,15 +1,8 @@
 module.exports = {
-  format_date: (date) => {
-    return `${new Date(date).getMonth() + 1}/${new Date(
+  format_date: date => {
+    return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(
       date
-    ).getDate()}/${new Date(date).getFullYear()}`;
-  },
-  format_plural: (word, amount) => {
-    if (amount !== 1) {
-      return `${word}s`;
-    }
-
-    return word;
+    ).getFullYear()}`;
   },
   format_url: url => {
     return url
@@ -18,5 +11,12 @@ module.exports = {
       .replace('www.', '')
       .split('/')[0]
       .split('?')[0];
+  },
+  format_plural: (word, amount) => {
+    if (amount !== 1) {
+      return `${word}s`;
+    }
+
+    return word;
   }
 };
